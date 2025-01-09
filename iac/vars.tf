d9a177b4e9f72a44724cf7207dcf515cce350ea8
Carl Paton <carl.paton@gmail.com>
2025-01-09 13:29:04 +1300

    add

--- iac/vars.tf
+++ iac/vars.tf
@@ -19,22 +19,16 @@ variable "location" {
 variable "subscription_id" {
   type        = string
   description = "The Azure subscription id"
-  # no default means it will need to be an input
-  #default = "00000000-0000-0000-0000-000000000000"
 }
 
 variable "sql_pass" {
   type        = string
   description = "The SQL Server password"
-  # no default means it will need to be an input
-  #default = "00000000-0000-0000-0000-000000000000"
 }
 
 variable "sql_user" {
   type        = string
   description = "The SQL Server user"
-  # no default means it will need to be an input
-  #default = "sqldemo-admin"
 }
 
 variable "group_key" {
